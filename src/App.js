84c5fe7bc2ecc462902999e9a5e92d48f6aa64b3
ManassehV7809 <104124845+ManassehV7809@users.noreply.github.com>
2023-06-25 16:18:21 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -10,78 +10,76 @@ function App() {
     <div className="App">
    <>
    <div className='introPic'>
-   <img src={corparate} alt="A picture of Vusani"></img>
+   <img src={corparate} alt="A picture of Vusani" style={{ maxWidth: '100%', height:'20%' }} />
+
 
 </div>
    <div className='intro'>
    <h2 strong>From me to you...</h2>
      
 
-<p>Ndau ya nduna! I'm Vusani Radzilani, a passionate individual with a strong affinity for the world of technology. Currently pursuing my studies in Computer Science, I am driven to [Goal or Mission Statement].
-
-Throughout my educational journey, I have acquired valuable expertise in various areas such as [Specific Skills or Expertise]. I hold steadfast to [Your Key Values or Principles] as guiding principles in everything I do.
-
-Welcome to my personal website, where you can delve into my professional background, explore the projects I have undertaken, and witness my unwavering commitment to continuous growth in the [Industry or Field]. Join me on this exciting journey as we uncover my achievements and aspirations.
-Thank you for taking the time to visit, and I sincerely hope you enjoy learning more about who I am and the value I bring.
-</p>
-<p>Best regards,</p>
-
-<p>Vusani Radzilani</p>
+   <div>
+      <h1>Ndau ya nduna!</h1>
+      <p>I'm Vusani Radzilani, a passionate individual with a strong affinity for the world of technology. Currently pursuing my studies in Computer Science, I am driven to stay at the forefront of innovation and make a positive impact in the field.</p>
+      <p>Throughout my educational journey, I have acquired valuable skills and knowledge in various areas, including programming languages such as Python, C++, and Java. I have also gained hands-on experience with technologies like ReactJS and Flutter, further expanding my technical toolkit.</p>
+      <p>As an adaptable and ambitious individual, I thrive in challenging environments and enjoy tackling complex problems. I believe that continuous learning and staying abreast of the latest industry trends are crucial for success in the ever-evolving world of technology.</p>
+      <p>On this website, you will find insights into my professional background, the projects I have undertaken, and my relentless pursuit of knowledge in the field of Computer Science. Join me on this exciting journey as we explore my accomplishments and aspirations.</p>
+      <p>Thank you for visiting, and I invite you to connect with me to discuss collaboration opportunities or share insights in the world of technology.</p>
+      <p>Ndo livhuwa,</p>
+      <p>Vusani Radzilani</p>
+    </div>
   
 
 </div>
 <div className="exp">
-<div card id="section-experience">
-<h2>Experience</h2>
-<ul>
-  <li>
-    <strong>Head Tutor | Wits University | Feb 2023 - Present</strong>
-    <ul>
-      <li>Teach first-year computer science students programming in Python and C++ as well as algorithms.</li>
-      <li>Ensure course quality control and fairness.</li>
-      <li>Foster a supportive learning environment that encourages questions and growth.</li>
-      <li>Regularly assess student progress and adjust teaching approach as needed.</li>
-    </ul>
-  </li>
-  <li>
-    <strong>GTS Tutor | Wits University | Feb 2023 - March 2023</strong>
-    <ul>
-      <li>Equip first-year students with basic computer and Microsoft 365 skills and LMS management.</li>
-      <li>Deliver a comprehensive program tailored to student needs.</li>
-      <li>Provide individualized support to help students overcome challenges and gain confidence.</li>
-      <li>Increased student satisfaction and set them up for success in their academic careers.</li>
-    </ul>
-  </li>
-</ul>
-</div>
-</div>
-
-<div className="skills">
-<div id="section-skills">
-<h2>Skills</h2>
-<ul>
-  <li>Strong technical skills:</li>
-  <ul>
-    <li>Python</li>
-    <li>C++</li>
-    <li>Java</li>
-    <li>Technologies such as ReactJS and Flutter</li>
-  </ul>
-  <li>Familiarity with software development tools and methodologies:</li>
-  <ul>
-    <li>Version control systems</li>
-    <li>Agile/Scrum</li>
-    <li>Testing</li>
-  </ul>
-  <li>Ability to work independently or as part of a team to solve complex problems and deliver high-quality software solutions.</li>
-  <li>Excellent communication and interpersonal skills, including the ability to explain technical concepts to non-technical stakeholders.</li>
-  <li>Adaptability and a willingness to learn and apply new technologies and frameworks as needed to stay current in the field.</li>
-</ul>
-
-<p>Please click <a href="./assets/resumeVusaniRadzilani.pdf" download>here </a>
-to download a pdf version of my resume.</p>
-</div>
-</div>
+      <div className="card" id="section-experience">
+        <h2>Experience</h2>
+        <ul>
+          <li>
+            <strong>Head Tutor | Wits University | Feb 2023 - Present</strong>
+            <ul>
+              <li>Instructed first-year computer science students in programming languages such as Python and C++, as well as algorithms.</li>
+              <li>Ensured course quality control and fairness.</li>
+              <li>Cultivated a supportive learning environment that fostered questioning and growth.</li>
+              <li>Regularly assessed student progress and adjusted teaching approach as needed.</li>
+            </ul>
+          </li>
+          <li>
+            <strong>GTS Tutor | Wits University | Feb 2023 - March 2023</strong>
+            <ul>
+              <li>Equipped first-year students with essential computer and Microsoft 365 skills, including LMS management.</li>
+              <li>Delivered a comprehensive program tailored to address individual student needs.</li>
+              <li>Provided personalized support to help students overcome challenges and build confidence.</li>
+              <li>Enhanced student satisfaction and facilitated their success in their academic careers.</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div className="skills">
+      <div id="section-skills">
+        <h2>Skills</h2>
+        <ul>
+          <li>Strong technical skills:</li>
+          <ul>
+            <li>Python</li>
+            <li>C++</li>
+            <li>Java</li>
+            <li>Technologies such as ReactJS and Flutter</li>
+          </ul>
+          <li>Familiarity with software development tools and methodologies:</li>
+          <ul>
+            <li>Version control systems</li>
+            <li>Agile/Scrum</li>
+            <li>Testing</li>
+          </ul>
+          <li>Ability to work independently or as part of a team to solve complex problems and deliver high-quality software solutions.</li>
+          <li>Excellent communication and interpersonal skills, including the ability to explain technical concepts to non-technical stakeholders.</li>
+          <li>Adaptability and a willingness to learn and apply new technologies and frameworks as needed to stay current in the field.</li>
+        </ul>
+        <p>Please click <a href="./assets/resumeVusaniRadzilani.pdf" download="resumeVusaniRadzilani.pdf">here</a> to download a PDF version of my resume.</p>
+      </div>
+    </div>
 <div className="contact">
 <div id="section-contact">
 <h2>Contact</h2>
