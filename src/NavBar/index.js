f9b17f26f6a11d73329f761e37d406540883fbfa
ManassehV7809 <104124845+ManassehV7809@users.noreply.github.com>
2023-06-26 01:34:45 +0200

    added icons

--- src/NavBar/index.js
+++ src/NavBar/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './index.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+
 
 function NavBar() {
   return (
@@ -8,15 +11,19 @@ function NavBar() {
       <nav className="navigation-bar">
         <ul className="navbar-list">
           <li className="nav-item">
-            <Link to="/" className="nav-link">
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/about" className="nav-link">
-              About
-            </Link>
-          </li>
+  <Link to="/" className="nav-link">
+    <FontAwesomeIcon icon={faHome} />
+    <span>Home</span>
+  </Link>
+</li>
+<li className="nav-item">
+  <Link to="/about" className="nav-link">
+    <FontAwesomeIcon icon={faInfoCircle} />
+    <span>About</span>
+  </Link>
+</li>
+
+
         </ul>
       </nav>
     </div>
